07e7b071803af9c12c92bc6017530b8dd8c344bd
Kitan-Dara06 <141774265+Kitan-Dara06@users.noreply.github.com>
2025-07-21 19:35:39 +0100

    Update llm_model.py

--- emotion_aware_assistant/services/llm_model.py
+++ emotion_aware_assistant/services/llm_model.py
@@ -1,5 +1,5 @@
-from gloabal_import import *
-from config import api_key
+from emotion_aware_assistant.gloabal_import import *
+from emotion_aware_assistant.config import api_key
 llm = ChatOpenAI(
     model="gpt-3.5-turbo",
     api_key=os.getenv("OPENAI_API_KEY"),
