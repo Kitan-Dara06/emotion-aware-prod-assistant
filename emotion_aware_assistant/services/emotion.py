daac8802eb5cce194e0aaa36bdcfa0b408494394
Kitan-Dara06 <141774265+Kitan-Dara06@users.noreply.github.com>
2025-07-21 21:19:09 +0100

    Update emotion.py

--- emotion_aware_assistant/services/emotion.py
+++ emotion_aware_assistant/services/emotion.py
@@ -1,13 +1,14 @@
 from emotion_aware_assistant.gloabal_import import *
 
-classifier = pipeline(
-    "text-classification",
-    model="SamLowe/roberta-base-go_emotions",
-    top_k=1,
-    truncation=True
-)
+# classifier = pipeline(
+#     "text-classification",
+#     model="SamLowe/roberta-base-go_emotions",
+#     top_k=1,
+#     truncation=True
+# )
 
 
-def detect_emotion(text):
-    return classifier(text)[0][0]['label']
+# def detect_emotion(text):
+#     return classifier(text)[0][0]['label']  switching to open ai classfication to reduce heaviness on deployment
 
+client = OpenAI(api_key=api_key)
