507aad8fd846e6beac7670667eb4d27e8c4464db
Kitan-Dara06 <141774265+Kitan-Dara06@users.noreply.github.com>
2025-07-23 10:43:43 +0100

    Update main.py

--- emotion_aware_assistant/main.py
+++ emotion_aware_assistant/main.py
@@ -105,7 +105,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://emotion-aware-assistant-frontend-18a79so02.vercel.app"],
+    allow_origins=["https://emotion-aware-assistant-frontend-iwkxz7do1.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
