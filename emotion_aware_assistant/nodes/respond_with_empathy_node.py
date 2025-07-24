6d4b83f33a97430d6513ead18de4beb2363b4fac
Kitan-Dara06 <141774265+Kitan-Dara06@users.noreply.github.com>
2025-07-24 23:22:39 +0100

    Update respond_with_empathy_node.py

--- emotion_aware_assistant/nodes/respond_with_empathy_node.py
+++ emotion_aware_assistant/nodes/respond_with_empathy_node.py
@@ -1,7 +1,7 @@
 from emotion_aware_assistant.services.assistant import respond_with_empathy
 from emotion_aware_assistant.utils.types import GraphState
 from emotion_aware_assistant.gloabal_import import *
-from emotion_aware_assistant.utils.ensure_state import ensure_graph_state 
+from emotion_aware_assistant.utils.ensure_graph_state import ensure_graph_state 
 
 def respond_with_empathy_node(state: GraphState) -> GraphState:
     state = ensure_graph_state(state)
