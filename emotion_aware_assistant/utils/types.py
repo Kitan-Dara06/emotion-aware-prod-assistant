115d68d7de9b12be1ed0871b8122fb7410f3e5cd
Kitan-Dara06 <141774265+Kitan-Dara06@users.noreply.github.com>
2025-07-24 13:26:58 +0100

    Update types.py

--- emotion_aware_assistant/utils/types.py
+++ emotion_aware_assistant/utils/types.py
@@ -1,17 +1,20 @@
 from emotion_aware_assistant.gloabal_import import *
 
-class GraphState(TypedDict):
+
+
+class GraphState(BaseModel):
     input: str
-    emotion: Optional[str]
-    goal: Optional[str]
-    suggested_action: Optional[str]
-    response: Optional[str]
+    emotion: Optional[str] = None
+    goal: Optional[str] = None
+    suggested_action: Optional[str] = None
+    response: Optional[str] = None
 
-    reminder: List[Dict[str, str]]
-    schedule_event: List[Dict[str, str]]
-    reschedule_event: List[Dict[str, str]]
+    reminder: List[Dict[str, str]] = []
+    schedule_event: List[Dict[str, str]] = []
+    reschedule_event: List[Dict[str, str]] = []
 
-    tool_result: Optional[str]
-    history: List[str]
-    emotion_history: List[Dict[str, str]]
-    user_profile: Optional[str]
+    tool_result: Optional[str] = None
+    history: List[str] = []
+    emotion_history: List[Dict[str, str]] = []
+    user_profile: Optional[str] = None
+
