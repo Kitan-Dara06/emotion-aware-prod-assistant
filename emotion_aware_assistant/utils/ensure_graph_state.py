a5db3c8d1d70078c1c44c372378e186ee3465863
Kitan-Dara06 <141774265+Kitan-Dara06@users.noreply.github.com>
2025-07-24 23:26:25 +0100

    Update ensure_graph_state.py

--- emotion_aware_assistant/utils/ensure_graph_state.py
+++ emotion_aware_assistant/utils/ensure_graph_state.py
@@ -1,4 +1,4 @@
 from emotion_aware_assistant.utils.types import GraphState
-from gloabal_import import *
+from emotion_aware_assistant.gloabal_import import *
 def ensure_graph_state(state):
     return GraphState(**state) if isinstance(state, dict) else state
